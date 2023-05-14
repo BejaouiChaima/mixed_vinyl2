d857f78a9cc3ddefcf7362d0e411fae0b1144258
BejaouiChaima <chaimabejaoui803@gmail.com>
2023-05-14 22:24:25 +0200

    commit

--- src/Entity/VinylMix.php
+++ src/Entity/VinylMix.php
@@ -13,8 +13,38 @@ class VinylMix
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $no = null;
+
+    #[ORM\Column]
+    private ?int $votes = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getNo(): ?string
+    {
+        return $this->no;
+    }
+
+    public function setNo(string $no): self
+    {
+        $this->no = $no;
+
+        return $this;
+    }
+
+    public function getVotes(): ?int
+    {
+        return $this->votes;
+    }
+
+    public function setVotes(int $votes): self
+    {
+        $this->votes = $votes;
+
+        return $this;
     }
 }
