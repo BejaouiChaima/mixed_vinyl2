c2f05a2bfd6adcb573f811953885ddef0fa8939d
BejaouiChaima <132885302+BejaouiChaima@users.noreply.github.com>
2023-05-15 11:23:27 +0200

    chapter 10

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -1,12 +1,16 @@
 <?php
 namespace App\Controller;
-use App\Service\MixRepository;
+use App\Repository\VinylMixRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 class VinylController extends AbstractController
 {
+    public function __construct(
+        private bool $isDebug
+    )
+    {}
     #[Route('/', name: 'app_homepage')]
     public function homepage(): Response
     {
@@ -24,11 +28,10 @@ class VinylController extends AbstractController
         ]);
     }
     #[Route('/browse/{slug}', name: 'app_browse')]
-    public function browse(MixRepository $mixRepository, string $slug = null): Response
+    public function browse(VinylMixRepository $mixRepository, string $slug = null): Response
     {
-        dd($this->getParameter('kernel.project_dir'));
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
-        $mixes = $mixRepository->findAll();
+        $mixes = $mixRepository->findAllOrderedByVotes();
         return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
             'mixes' => $mixes,
